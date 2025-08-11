06d772c75a40382dc1808feabe08c8e85142da05
voidf <510030805@qq.com>
2025-08-11 22:38:28 +0800

    传一下临时修改

--- SRC/PRigid.cs
+++ SRC/PRigid.cs
@@ -3,77 +3,93 @@ using System;
 
 public partial class PRigid : RigidBody2D
 {
-    [Export]
-    public float EXP_VerticalV = 2000f;
-    [Export]
-    public float EXP_HorizonV = 1000f;
-    [Export]
-    public AnimatedSprite2D EXP_anim;
+	[Export]
+	public float EXP_VerticalV = 2000f;
+	[Export]
+	public float EXP_HorizonV = 1000f;
+	[Export]
+	public AnimatedSprite2D EXP_anim;
 
-    public override void _IntegrateForces(PhysicsDirectBodyState2D state)
-    {
-        base._IntegrateForces(state);
-        var v = state.LinearVelocity;
-        var step = state.Step;
-        var inputAxis = Vector2.Zero;
+	public override void _Input(InputEvent @event)
+	{
+		// Mouse in viewport coordinates.
+		if (@event is InputEventMouseButton eventMouseButton)
+		{
+			GD.Print("Mouse Click/Unclick at: ", eventMouseButton.Position);
+		}
+		else if (@event is InputEventMouseMotion eventMouseMotion)
+		{
+			GD.Print("Mouse Motion at: ", eventMouseMotion.Position);
+		}
 
-        if (Input.IsActionPressed("MovD"))
-        {
-            inputAxis.X += EXP_HorizonV;
-            EXP_anim.FlipH = true;
-        }
+		// Print the size of the viewport.
+		GD.Print("Viewport Resolution is: ", GetViewport().GetVisibleRect().Size);
+	}
 
-        if (Input.IsActionPressed("MovA"))
-        {
-            inputAxis.X -= EXP_HorizonV;
-            EXP_anim.FlipH = false;
-        }
+	public override void _IntegrateForces(PhysicsDirectBodyState2D state)
+	{
+		base._IntegrateForces(state);
+		var v = state.LinearVelocity;
+		var step = state.Step;
+		var inputAxis = Vector2.Zero;
 
-        if (Input.IsActionPressed("Jmp"))
-        {
-            inputAxis.Y -= EXP_VerticalV;
-        }
+		if (Input.IsActionPressed("MovD"))
+		{
+			inputAxis.X += EXP_HorizonV;
+			EXP_anim.FlipH = true;
+		}
 
-        int floor_idx = -1;
-        for (int i = 0; i < state.GetContactCount(); ++i)
-        {
-            var cNorm = state.GetContactLocalNormal(i);
-            if (cNorm.Dot(new Vector2(0, -1)) > .6f)
-            {
-                floor_idx = i;
-            }
-        }
+		if (Input.IsActionPressed("MovA"))
+		{
+			inputAxis.X -= EXP_HorizonV;
+			EXP_anim.FlipH = false;
+		}
 
-        if (inputAxis.LengthSquared() > 0)
-        {
-            var nv = inputAxis;
+		if (Input.IsActionPressed("Jmp"))
+		{
+			inputAxis.Y -= EXP_VerticalV;
+		}
 
-        }
+		int floor_idx = -1;
+		for (int i = 0; i < state.GetContactCount(); ++i)
+		{
+			var cNorm = state.GetContactLocalNormal(i);
+			if (cNorm.Dot(new Vector2(0, -1)) > .6f)
+			{
+				floor_idx = i;
+			}
+		}
 
+		if (inputAxis.LengthSquared() > 0)
+		{
+			var nv = inputAxis;
 
-    }
-    public override void _Ready()
-    {
-        base._Ready();
-        EXP_anim.Play();
-    }
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
-        // Rotation += 1f * (float)delta;
-
-
-        // if (Input.IsActionPressed("MovW"))
-        //     v.Y -= 1;
-
-        // if (Input.IsActionPressed("MovS"))
-        //     v.Y += 1;
+		}
 
 
-        // Position = new Vector2(
-        //     Mathf.Clamp(Position.X + v.X * EXP_MoveSpeed * (float)delta, 0, GetViewportRect().Size.X),
-        //     Mathf.Clamp(Position.Y + v.Y * EXP_MoveSpeed * (float)delta, 0, GetViewportRect().Size.Y)
-        // );
-    }
+	}
+	public override void _Ready()
+	{
+		base._Ready();
+		EXP_anim.Play();
+	}
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+		// Rotation += 1f * (float)delta;
+
+
+		// if (Input.IsActionPressed("MovW"))
+		//     v.Y -= 1;
+
+		// if (Input.IsActionPressed("MovS"))
+		//     v.Y += 1;
+
+
+		// Position = new Vector2(
+		//     Mathf.Clamp(Position.X + v.X * EXP_MoveSpeed * (float)delta, 0, GetViewportRect().Size.X),
+		//     Mathf.Clamp(Position.Y + v.Y * EXP_MoveSpeed * (float)delta, 0, GetViewportRect().Size.Y)
+		// );
+	}
 
 }
